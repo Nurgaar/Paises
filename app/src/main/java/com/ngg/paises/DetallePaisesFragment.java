794692fc420674bd9d4c79b872eab9115e2c2380
Nurgaar <nuritagarzon@gmail.com>
2021-11-11 10:42:30 +0100

    Initial commit

---
+++ app/src/main/java/com/ngg/paises/DetallePaisesFragment.java
@@ -0,0 +1,46 @@
+package com.ngg.paises;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ngg.android.paises.placeholder.PlaceholderContent;
+import com.ngg.paises.databinding.FragmentDetallePaisesBinding;
+
+
+
+public class DetallePaisesFragment extends Fragment {
+
+   private FragmentDetallePaisesBinding binding;
+   private PlaceholderContent.Pais mPais;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mPais = getArguments().getParcelable("pais");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentDetallePaisesBinding.inflate(inflater,container,false);
+
+        ImageView iv = binding.foto;
+        iv.setImageResource(getImageId(mPais.foto));
+        TextView tv = binding.descripcion;
+        tv.setText(mPais.detalles);
+
+        return binding.getRoot();
+    }
+
+    private int getImageId(String imagePath){
+        String imageName = imagePath.substring(imagePath.lastIndexOf("/")+1,imagePath.lastIndexOf("."));
+        return getResources().getIdentifier(imageName,"drawable",getContext().getPackageName());
+    }
+}
